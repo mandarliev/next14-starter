7d35d4ffcee3ef9443c2959d3f72bb4774f48d92
Dimitar <dimitar.mandarliev@gmail.com>
2024-02-25 16:14:22 +0200

    Image Explained

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Image from "next/image";
+import styles from "./about.module.css";
 
 function AboutPage() {
   return (
-    <div>AboutPage</div>
-  )
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/1802268/pexels-photo-1802268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt="about"
+          fill
+        />
+      </div>
+    </div>
+  );
 }
 
-export default AboutPage
+export default AboutPage;
