055a9a3a10a96ad4b34143821ae8a5afaa9ddbbe
Dimitar <dimitar.mandarliev@gmail.com>
2024-02-25 17:04:33 +0200

    TITLE BABY

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import Footer from "./components/footer/footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Next App",
+  title: "Dashboard",
   description: "Next.js starter app",
 };
 
