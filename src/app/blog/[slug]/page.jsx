ba3f39df88351bd7031936da38ef09eb383f4702
Dimitar <dimitar.mandarliev@gmail.com>
2024-02-26 15:34:33 +0200

    Single Blog Page Design

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import Image from "next/image";
+import styles from "./singlePost.module.css";
 
 function SinglePostPage() {
   return (
-    <div>SinglePostPage</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/1802268/pexels-photo-1802268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          fill
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            className={styles.avatar}
+            width={50}
+            height={50}
+            src="https://images.pexels.com/photos/1802268/pexels-photo-1802268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Dimitar Mandarliev</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur,
+          praesentium explicabo. Ducimus, nisi quod. Facilis illum nihil
+          voluptate molestias, tenetur, quae voluptates voluptas ullam aperiam
+          ab pariatur dignissimos repellendus ratione.
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SinglePostPage
+export default SinglePostPage;
